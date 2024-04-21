dc4e7adedccfe0529d70504c40c815c638f49cc9
ROXYCHEN <roxy@maicoin.com>
2024-04-21 21:47:38 +0800

    added passage

--- App.tsx
+++ App.tsx
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, useCallback } from 'react';
+import { TextInput, Button, StyleSheet, Text, View } from 'react-native';
+import { TokenStore, Passage } from '@passageidentity/passage-js';
+
+// import Passage, {
+//   PassageUser,
+//   AllowedFallbackAuth,
+//   PassageError,
+//   PassageErrorCode,
+// } from '@passageidentity/passage-react-native';
+
+const passage = new Passage('y4WGlmVdLzmjqF9TDcOhrQb2');
 
 export default function App() {
+  const [email, setEmail] = useState('roxy@maicoin.com');
+
+  const handleRegisterPasskey = useCallback(async () => {
+   try {
+    const result = await passage.register(email);
+    console.log(result);
+    } catch(err) {
+      console.log(err);
+    }
+  }, [email]);
+
+  const handlePasskeyLogin = useCallback(async () => {
+    try {
+    const result = await passage.login(email);
+    console.log(result);
+    } catch(err) {
+      console.log(err);
+    }
+  }, [email]);
+
+
+  const handleTextInputChange = useCallback((value: string)=>{
+    console.log(value);
+    setEmail(value);
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button onPress={handlePasskeyLogin} title="Login With Passkey"/>
+      <View style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <TextInput style={{width: 200, height: 30, marginVertical: 8, textAlign: 'center'}} onChangeText={handleTextInputChange} value={email}/>
+      </View>
+      <Button onPress={handleRegisterPasskey} title="Register With Passkey"/>
       <StatusBar style="auto" />
     </View>
   );
